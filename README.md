303d68d48de62df151cc5808fbdfaf75bda73856
aditya-an1l <aditya.anil.productions@gmail.com>
2024-06-09 03:09:55 +0530

    first commmit

---
+++ README.md
@@ -0,0 +1,3 @@
+<center><h1>My NeoVim Conig files</h1></center>
+
+This is my [LazyVim](https://www.lazyvim.org/) Neovim Config I use predominantly coding and programming tasks.
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("i", "<C-H>", "<C-W>", { desc = "Ctrl Delete to delete the previous word" })
+
+
