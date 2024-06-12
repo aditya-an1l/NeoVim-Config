209fa9f404fbe5b787893c983c2583270c2144f3
aditya-an1l <aditya.anil.productions@gmail.com>
2024-06-12 16:34:22 +0530

    keymaps modified

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "<C-a>", "ggVG", { desc = "Select All" })
+-- Map Ctrl-A to select all text in normal mode
+vim.keymap.set({ "i", "n" }, "<C-A>", "<Esc>ggVG")
