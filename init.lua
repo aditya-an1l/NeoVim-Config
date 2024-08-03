c5f203d3efaa47efe6bccf2199d30df1882d9f1a
aditya-an1l <aditya.anil.productions@gmail.com>
2024-08-03 13:52:47 +0530

    Add option to open neo tree in the right position IN init.lua

--- init.lua
+++ init.lua
@@ -6,3 +6,7 @@ require("nvim-treesitter.install").compilers = { "zig", "clang" }
 
 -- setting powershell inside of nvim
 vim.o.shell = "pwsh.exe"
+
+-- intialise the neotree on the right
+
+require("neo-tree.command").execute({ position = "right" })
