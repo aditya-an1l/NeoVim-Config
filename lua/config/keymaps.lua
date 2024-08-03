b460155fbd841d9a8ab244bbc2b9129d4e008632
aditya-an1l <aditya.anil.productions@gmail.com>
2024-08-03 13:50:48 +0530

    Add 'insert mode keymaps AND remapped the undo function" in keymap.lua
    Changes
    - Added KJ as an option to go to insert mode and out of insert mode
    
    added
    - remap to undo using U

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,11 +7,14 @@ vim.keymap.set("n", "<C-\\>", "a<C-W><Esc>", { desc = "Delete previous word" })
 
 vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
 
-vim.keymap.set("i", "lkj", "<Esc>", { desc = "Go to Insert Mode" })
-vim.keymap.set("n", "lkj", "i", { desc = "Go to Insert Mode" })
+vim.keymap.set("i", "lkj", "<Esc>", { desc = "Go to Insert Mode #1" })
+vim.keymap.set("n", "lkj", "i", { desc = "Go to Insert Mode #1" })
+vim.keymap.set("i", "KJ", "<Esc>", { desc = "Go to Insert Mode #2" })
+vim.keymap.set("n", "KJ", "i", { desc = "Go to Insert Mode #2" })
+
+vim.keymap.set("n", "U", "<C-r>", { desc = "Redo" })
 
 vim.keymap.set("n", "j", "jzz", { desc = "Cursor at the centre of the screen while pressing j" })
-
 vim.keymap.set("n", "k", "kzz", { desc = "Cursor at the centre of the screen while pressing k" })
 
 vim.keymap.set("i", "<Space>e", function()
@@ -22,4 +25,6 @@ vim.keymap.set("i", "<Space>e", function()
   })
 end, { desc = "NeoTree" })
 
-vim.keymap.set("n", "D", '"_dd', { desc = "Delete without adding to buffer" })
+vim.keymap.set("n", "<leader>d", '"_dd', { desc = "Delete without adding to buffer" })
+
+vim.keymap.set("n", " <leader>p", '"_dP', { desc = "Delete the selection WITHOUT resetting the buffer" })
