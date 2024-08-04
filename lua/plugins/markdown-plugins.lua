e989d989c55ca13941ff23e2c60376e0878f3f0f
aditya-an1l <aditya.anil.productions@gmail.com>
2024-08-04 23:42:56 +0530

    Add markdown-preview plugin markdown-plugins.lua

--- lua/plugins/markdown-plugins.lua
+++ lua/plugins/markdown-plugins.lua
@@ -1,4 +1,14 @@
 return {
-   "SidOfc/mkdx", 
-  ft = "markdown" 
+  { "SidOfc/mkdx", ft = "markdown" },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+    enabled = true,
+    lazy = true,
+  },
 }
