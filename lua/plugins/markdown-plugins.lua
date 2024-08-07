b932398d368cc13e706a4ace09956084b9d40797
aditya-an1l <aditya.anil.productions@gmail.com>
2024-08-07 10:35:24 +0530

    Replaced MarkdownPreview with Peek plugin in markdown-plugin.lua
    Reason for replacement
    - MarkdownPreview affects startup time, and it bit bloated for my
    environemnt
    - Peek runs on deno, so its naturally fast, and runs on Web-based GUI
    using Deno

--- lua/plugins/markdown-plugins.lua
+++ lua/plugins/markdown-plugins.lua
@@ -1,14 +1,13 @@
 return {
   { "SidOfc/mkdx", ft = "markdown" },
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
     end,
-    ft = { "markdown" },
-    enabled = true,
-    lazy = true,
   },
 }
