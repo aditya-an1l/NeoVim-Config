e7a68404252da4983e684dc97ff85a62516596d1
aditya-an1l <aditya.anil.productions@gmail.com>
2025-03-30 14:03:19 +0530

    Change custom options
    I am using Arch now -- so this is not needed

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -13,48 +13,4 @@ return {
       desc = "Format Injected Langs",
     },
   },
-
-  opts = {
-    formatters = {
-      -- In windows, I have space in my User Name space, and this thing cannot escape the space for some reasong.
-      -- I put the cmd files into C:\\Users file, and manually setting the path
-      -- changed the path of the conform cmd.
-      stylua = {
-        command = "C:\\Users\\nvim\\nvim-conform\\stylua.cmd",
-      },
-      java = {
-        command = "C:\\Users\\nvim\\nvim-conform\\java.cmd",
-      },
-
-      prettier = {
-        command = "C:\\Users\\nvim\\nvim-conform\\prettier.cmd",
-      },
-
-      prettierd = {
-        command = "C:\\Users\\nvim\\nvim-conform\\prettierd.cmd",
-      },
-      shfmt = {
-        command = "C:\\Users\\nvim\\nvim-conform\\shfmt.cmd",
-      },
-
-      biome = {
-        command = "C:\\Users\\nvim\\nvim-conform\\biome.cmd",
-      },
-
-      ruff = {
-        command = "C:\\Users\\nvim\\nvim-conform\\ruff.cmd",
-      },
-
-      clangformat = {
-        command = "C:\\Users\\nvim\\nvim-conform\\clang-format.cmd",
-      },
-
-      markdownlint = {
-        command = "C:\\Users\\nvim\\nvim-conform\\markdownlint.cmd",
-      },
-      jdtls = {
-        command = "C:\\Users\\nvim\\nvim-conform\\jdtls.cmd",
-      },
-    },
-  },
 }
