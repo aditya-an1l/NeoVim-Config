c199ca038197dc096954f545acc7defefbb74d2c
aditya-an1l <aditya.anil.productions@gmail.com>
2025-03-30 14:04:11 +0530

    New mode + lazy-lock commit

--- init.lua
+++ init.lua
@@ -1,11 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- require("lazy").setup({ { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" } })
-require("nvim-treesitter.install").compilers = { "zig", "clang" }
-
--- setting powershell inside of nvim
-vim.o.shell = "pwsh.exe"
-
--- center the cursor
-vim.opt.scrolloff = 999
